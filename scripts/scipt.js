46136d7132766175194cf965f9ec39717dfe57a1
mffdb <70085399+mffdb@users.noreply.github.com>
2020-09-09 15:33:15 +0900

    Update scipt.js

--- scripts/scipt.js
+++ scripts/scipt.js
@@ -1,5 +1,5 @@
 $(document).ready(function(event){
-   $('[id^="btn"]')
+/*   $('[id^="btn"]')
     .css("cursor", "pointer")
     .attr("title", "열기")
     .click(function(){    
@@ -18,6 +18,19 @@ $(document).ready(function(event){
    $('.tr-' + name).toggleClass("selected");
     $(this).toggleClass("selected");
   })
+*/
+ $( "[id^=btn]" ).click(function() {
+ /*  var html = [ "The clicked div has the following styles:" ];
+
+     var styleProps = $( this ).css(["width", "height", "color", "background-color", "background-image"]);
+      $.each( styleProps, function( prop, value ) {
+        html.push( prop + ": " + value );
+      });
+      */
+     var name = $(this).attr("name");
+      $( ".result1" ).css({"background-image": "url("+name+".jpg)"});
+  /*  $( ".result2" ).html( html.join( "<br>" ) );*/
+    });
 
  event.preventDefault();
 })
