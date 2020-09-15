fe1cf9449b4edeb2734fdace58986f47da2ccff0
mffdb <70085399+mffdb@users.noreply.github.com>
2020-09-15 13:19:18 +0900

    Update scipt.js

--- scripts/scipt.js
+++ scripts/scipt.js
@@ -1,41 +1,33 @@
-$(document).ready(function(event){
-/*   $('[id^="btn"]')
-    .css("cursor", "pointer")
-    .attr("title", "열기")
-    .click(function(){    
-   var name = $(this).attr("name");
-    $('[id^="btn"]').removeClass('selected');
-    $('[class^="content"]').removeClass('selected');
-    $(this).addClass('selected');
-    $(".content-" + name).addClass('selected');
-  })
-  
-   $('[id^="toggle"]')
-    .css("cursor", "pointer")
-    .attr("title", "열기")
-    .click(function(){    
-   var name = $(this).attr("name");
-   $('.tr-' + name).toggleClass("selected");
-    $(this).toggleClass("selected");
-  })
-*/
- $( "[id^=btn]" ).css({"cursor", "pointer"}).click(function() {
- /*  var html = [ "The clicked div has the following styles:" ];
+$( "[id^=btn]" ).css({"cursor": "pointer"}).click(function() {
+var name = $(this).attr("name");
+$('[id^="btn"]').removeClass('selected');
+$( ".portrait" ).css({"background-image": "url(./portraits/"+name+".png)"}).removeClass('selected');
 
-     var styleProps = $( this ).css(["width", "height", "color", "background-color", "background-image"]);
-      $.each( styleProps, function( prop, value ) {
-        html.push( prop + ": " + value );
-      });
-      
-    var name = $(this).attr("name");
-    $('[id^="btn"]').removeClass('selected');
-    $( ".portrait" ).css({"background-image": "url(../portraits/"+name+".png)"}).removeClass('selected');
-    $(this).addClass('selected');
-    $(".portrait").css({"background-image": "url(../portraits/"+name+".png)"}).addClass('selected')
-    $( ".result" ).html( html.join( "<br>" ) );*/
-    });
+$(this).addClass('selected');
+$(".portrait").css({"background-image": "url(./portraits/"+name+".png)"}).addClass('selected');
 
- event.preventDefault();
-})
+var idcode = []; 
+var idtype = [];
+var idgender = [];
+var idside = [];
+var figure = [
+  { id: "hero_adamwarlock01", type: "컴뱃", gender: "남", side: "악당", allies: "인간", ability: "타임 프리징, 마법", team: "어벤져스", utilities: "잠재력, T3" }, 
+  { id: "hero_aero01", type: "스피드", gender: "여", side: "영웅", allies: "인간", ability: "타임 프리징, 마법", team: "어벤져스", utilities: "잠재력, T3" }, 
 
-  
+];
+
+
+$.each(figure, function(idx, obj) {    
+  if (name == obj.id) {
+    idtype.push( obj.id );
+    idgender.push( obj.type );
+    idside.push( obj.side );
+
+  }
+}); 
+
+$(".type").text( idtype.join( "" ) );
+$(".genter").text( idgender.join( "" ) );
+$(".side").text( idside.join( "" ) );
+    
+});
