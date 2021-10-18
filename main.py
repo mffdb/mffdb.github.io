84d8c85eae8b2081ba6f6dcebf21932aa205659c
mffdb <70085399+mffdb@users.noreply.github.com>
2021-10-18 17:07:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import json
-import sys
 from datetime import datetime
 from pytz import timezone
 #from crawling_yes24 import parsing_beautifulsoup, extract_book_data	# crawling_yes24.py에서 parsing_beautifulsoup, extract_book_data 함수 불러오기
@@ -18,18 +17,12 @@ if __name__ == "__main__":
     today_date = today.strftime("%Y년 %m월 %d일")
 
     yes24_it_new_product_url = "http://www.yes24.com/24/Category/NewProductList/001001003?sumGb=01"
-    
-    url = 'https://kleague.com/api/clubRank.do'
 
 #    soup = parsing_beautifulsoup(yes24_it_new_product_url)
 
     data = requests.get(yes24_it_new_product_url)
     html = data.text
     soup = BeautifulSoup(html, 'html.parser')
-
-    req1 = requests.get(url)
-    html1 = req1.content
-    soup1 = BeautifulSoup(html, 'html.parser')
 
 #    upload_contents = extract_book_data(soup)
 
@@ -46,32 +39,17 @@ if __name__ == "__main__":
         content = f"<a href={url}>" + book_name + "</a>" + ", " + price + "<br/>\n"
         upload_contents += content
 
-    rank_json=json.loads(soup1.text)
-    rank1 = []
-    rank2 = []   
-    rank1 = rank_json["data"]["league1"]
-    rank2 = rank_json["data"]["league2"]
-    rank_zip = rank1 + rank2
-
-    league_ranking = json.dumps(rank_zip, ensure_ascii=False, indent="\t")
-    
 #    repo = get_github_repo(access_token, repository_name)
 
     g = Github(access_token)
     repo = g.get_user().get_repo(repository_name)
 
     issue_title = f"YES24 IT 신간 도서 알림({today_date})"
-    
-    repo1 = g.get_user().get_repo(repository_name)
-    
-    issue_title1 = f"K리그 순위({today_date})"
 
     
 #    upload_github_issue(repo, issue_title, upload_contents)
 
     repo.create_issue(title=issue_title, body=upload_contents)
-       
-    repo1.create_issue(title=issue_title, body=league_ranking)
-    repo1.create_file("rank.json", "commit message", league_ranking)
+    repo.create_file("rank.json", "commit message", upload_contents)
        
     print("Upload Github Issue Success!")
